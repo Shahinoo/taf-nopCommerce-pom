472c50d81a2605638f5dd7cbe5e700d4c81890b9
muhammad shaheen <mohammed_shaheen22@yahoo.com>
2021-11-19 22:52:35 +0200

    Implement Tests of HoverMenu

--- src/test/java/scripts/HoverMenuTest.java
+++ src/test/java/scripts/HoverMenuTest.java
@@ -1,5 +1,20 @@
 package scripts;
 
-public class HoverMenuTest extends TestBase {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
 
+public class HoverMenuTest extends TestBase {
+    HomePage homePage;
+
+
+    @Test
+    public void userSelectSubcategoryFromMenu() {
+        homePage = new HomePage(driver);
+        homePage.selectNotebooksMenu();
+        //Verify
+        System.out.println(driver.getCurrentUrl());
+        //Assert
+        Assert.assertTrue(driver.getCurrentUrl().contains("notebooks"));
+    }
 }
